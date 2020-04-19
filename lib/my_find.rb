ee1f1284d1382c48abc55d8123cad7c723a1a46a
Marcus Lau <lau.marcus76@gmail.com>
2020-04-19 02:51:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,7 @@
-require 'pry'
-
 def my_find(collection)
-  i = 0 
-  while i < collection.length 
-    if yield(collection[i])
-      return
-    end
-    i+=1 
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
   end
 end
